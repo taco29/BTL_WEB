2bd4c7f4ced77c2f59ca89026e74c6e8dc7ab5ac
Vũ Huy <huyvu292007@gmail.com>
2026-06-06 09:44:37 +0700

    change embedding model

--- chatbot/embedding.py
+++ chatbot/embedding.py
@@ -3,7 +3,7 @@ from langchain_huggingface import HuggingFaceEmbeddings
 class EmbeddingModel:
     def __init__(
         self,
-        model_name: str = 'keepitreal/vietnamese-sbert',
+        model_name: str = 'sentence-transformers/all-MiniLM-L6-v2',
         normalize: bool = True
     ):
         self.model_name = model_name
