5e635450922943ec7f6457de819fe8babf6d2165
Vũ Huy <huyvu292007@gmail.com>
2026-06-06 09:13:48 +0700

    feat: chunking for each major, header1

--- chatbot/chunking.py
+++ chatbot/chunking.py
@@ -22,6 +22,33 @@ class TextChunker:
         final_splits = self.text_splitter.split_documents(header_splits)
         return final_splits
 
+    def chunking_for_each_major(self, text: str) ->list:
+        """
+        - section ngành : mỗi ngành 1 dòng
+        - còn lại: recursive
+        """
+        header_splits = self.header_splitter.split_text(text)
+        final_docs = []
+
+        for doc in header_splits:
+            lines = doc.page_content.split("\n")
+            list_lines =[l for l in lines if l.strip().startswith("- -")]
+
+            if len(list_lines) >= 3:
+                intro_lines = []
+                for line in lines:
+                    if line.strip().startswith("- -"):
+                        break
+                    intro_lines.append(line)
+                intro = '\n'.join(intro_lines).strip()
+
+                for line in list_lines:
+                    content = f"{intro}\n{line}".strip() if intro else line
+                    final_docs.append(Document(page_content=content, metadata= doc.metadata))
+            else:
+                sub_docs = self.text_splitter.split_documents([doc])
+                final_docs.extend(sub_docs)
+        return final_docs
 # chunker = TextChunker()
 # text_data = chunker.load_docs("./data/data.md")
 # docs = chunker.chunking(text_data)
@@ -33,3 +60,14 @@ class TextChunker:
 #     print("\n noi dung:")
 #     print(doc.page_content)
 #     print("\n\n")
+# from langchain_core.documents import Document
+# chunker = TextChunker()
+# text = chunker.load_docs(r"C:\Users\Admin\Code\webweb\data\data.md")
+# docs = chunker.chunking_for_each_major(text)
+
+# for i, doc in enumerate(docs):
+#     if 'UDU' in doc.page_content or 'định hướng ứng dụng' in doc.page_content.lower():
+#         print(f"--- CHUNK {i} ---")
+#         print("METADATA:", doc.metadata)
+#         print("CONTENT:", doc.page_content)
+#         print()
